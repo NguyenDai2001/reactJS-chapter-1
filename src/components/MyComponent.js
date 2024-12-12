46eac81b83b589405eb2c78c8a36009d18238451
NguyenDai2001 <dai242001@gmail.com>
2024-12-12 15:35:17 +0700

    2

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+class MyComponent extends react.Component {
+
+    render() {
+        return (
+            <div>
+                mmama: {Math.random(10)}
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
